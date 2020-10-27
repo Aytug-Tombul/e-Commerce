98871c4e2f8626a7809120f88f4011c8de2389ad
Aytug-tombul <aytugtombul@gmail.com>
2020-10-28 00:13:15 +0300

    first things

---
+++ app/controllers/Pages.php
@@ -0,0 +1,13 @@
+<?php 
+class Pages{
+    public function __construct() {
+
+    }
+
+    public function index(){
+        echo 'homepage';
+    }
+    public function about(){
+        echo 'about';
+    }
+}
